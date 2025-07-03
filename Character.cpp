d3f04eaf9d138257b4b06007ae2473d3c2780474
shaoyu <fanshaoyu9@outlook.com>
2025-07-03 18:54:06 -0400

    Create character base class

---
+++ Character.cpp
@@ -0,0 +1,89 @@
+#include "Character.h"
+#include "CollisionManager.h"
+
+Character::Character() {
+    p_hitBox = CollisionManager::instance()->createCollisionBox();
+    p_hurtBox = CollisionManager::instance()->createCollisionBox();
+
+    p_timerInvulnerableStatus.setDuration(1.0f);
+    p_timerInvulnerableStatus.setOneShot(true);
+    p_timerInvulnerableStatus.setOnTimeOut(
+        [&]() {
+            p_isInvulnerable = false;
+        }
+    );
+
+    p_timerInvulnerableBlink.setDuration(0.075f);
+    p_timerInvulnerableBlink.setOneShot(false);
+    p_timerInvulnerableBlink.setOnTimeOut(
+        [&]() {
+            p_isBlinkInvisible = !p_isBlinkInvisible;
+        }
+    );
+}
+
+Character::~Character() {
+    CollisionManager::instance()->destroyCollisionBox(p_hitBox);
+    CollisionManager::instance()->destroyCollisionBox(p_hurtBox);
+}
+
+void Character::decreaseHp() {
+    if (p_isInvulnerable) return;
+
+    p_hp -= 1;
+    if (p_hp > 0) makeInvulnerable();
+    onHurt();
+}
+
+void Character::onInput(const SDL_Event& event) {}
+
+void Character::onUpdate(float deltaTime) {
+    p_stateMachine.onUpdate(deltaTime);
+
+    if (p_hp <=0) p_velocity.x = 0;
+    if (p_shouldEnableGravity) p_velocity.y += GRAVITY_Y * deltaTime;
+
+    p_position += p_velocity * deltaTime;
+
+    if (isBelowFloor()) {
+        p_position.y = FLOOR_Y;
+        p_velocity.y = 0; 
+    }
+
+    if (p_position.x <= 0) p_position.x = 0;
+    if (p_position.x >= 1280) p_position.x = 1280.0f;
+
+    p_hurtBox->setPosition(getLogicCenter());
+
+    p_timerInvulnerableStatus.update(deltaTime);
+
+    if (p_isInvulnerable) p_timerInvulnerableBlink.update(deltaTime);
+
+    if (!p_currentAnimation) return;
+
+    Animation& animation = (
+        p_isFacingLeft ? p_currentAnimation->left : p_currentAnimation->right
+    );
+    animation.update(deltaTime);
+    animation.setPosition(p_position);
+}
+
+void Character::render(const Camera& camera) {
+    if (!p_currentAnimation || (p_isInvulnerable && p_isBlinkInvisible)) return;
+
+    (p_isFacingLeft ? p_currentAnimation->left : p_currentAnimation->right).render(camera);
+}
+
+
+void Character::onHurt() {}
+
+void Character::switchState(const std::string& id) {
+    p_stateMachine.switchTo(id);
+}
+
+void Character::setAnimation(const std::string& id) {
+    p_currentAnimation = &p_animationPool[id];
+
+    p_currentAnimation->left.reset();
+    p_currentAnimation->right.reset();
+}
---
+++ Character.h
@@ -0,0 +1,107 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "Vector2.h"
+#include "Animation.h"
+#include "CollisionBox.h"
+#include "StateMachine.h"
+
+#include <SDL2/SDL.h>
+
+#include <SDL2/SDL_events.h>
+#include <string>
+#include <unordered_map>
+
+class Character {
+public:
+    Character();
+    ~Character();
+
+    void decreaseHp();
+
+    int getHp() const {
+        return p_hp;
+    }
+
+    void setPosition(const Vector2& position) {
+        p_position = position;
+    }
+
+    const Vector2& getPosition() const {
+        return p_position;
+    }
+
+    void setVelocity(const Vector2& velocity) {
+        p_velocity = velocity;
+    }
+
+    const Vector2& getVelocity() const {
+        return p_velocity;
+    }
+
+    Vector2 getLogicCenter() const {
+        return Vector2(p_position.x, p_position.y - p_logicHeight / 2);
+    }
+
+    void setGravityEnabled(bool flag) {
+        p_shouldEnableGravity = flag;
+    }
+
+    CollisionBox* getHitBox() {
+        return p_hitBox;
+    }
+
+    CollisionBox* getHurtBox() {
+        return p_hurtBox;
+    }
+
+    bool isBelowFloor() const {
+        return p_position.y > FLOOR_Y; // >=
+    }
+
+    float getFloorY() const {
+            return FLOOR_Y;
+    }
+
+    void makeInvulnerable() {
+        p_isInvulnerable = true;
+        p_timerInvulnerableStatus.restart();
+    }
+
+    virtual void onInput(const SDL_Event& event);
+    virtual void onUpdate(float deltaTime);
+    virtual void render(const Camera& camera);
+
+    virtual void onHurt();
+
+    void switchState(const std::string& id);
+    void setAnimation(const std::string& id);
+
+protected:
+    struct AnimationGroup {
+        Animation left;
+        Animation right;
+    };
+
+    const float FLOOR_Y = 620;
+    const float GRAVITY_Y = 980;
+
+    int p_hp = 10;
+    Vector2 p_position;
+    Vector2 p_velocity;
+    float p_logicHeight = 0;
+    bool p_isFacingLeft = true;
+    StateMachine p_stateMachine;
+    bool p_shouldEnableGravity = true;
+    bool p_isInvulnerable = false;
+    Timer p_timerInvulnerableBlink;
+    Timer p_timerInvulnerableStatus;
+    bool p_isBlinkInvisible = false;
+    CollisionBox* p_hitBox = nullptr;
+    CollisionBox* p_hurtBox = nullptr;
+    AnimationGroup* p_currentAnimation = nullptr;
+    std::unordered_map< std::string, AnimationGroup> p_animationPool;
+
+};
+
+#endif
