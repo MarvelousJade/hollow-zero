f938e0ca561e5dcf8fe43791ef48ae29d3347230
shaoyu <fanshaoyu9@outlook.com>
2025-07-09 23:30:55 -0400

    Create methods of character manager for boss enemy

--- CharacterManager.cpp
+++ CharacterManager.cpp
@@ -1,3 +1,4 @@
+#include "Enemy.h"
 #include "Player.h"
 #include "CharacterManager.h"
 #include "BulletTimeManager.h"
@@ -15,15 +16,18 @@ void CharacterManager::onInput(const SDL_Event& e) {
 }
 
 void CharacterManager::update(float deltaTime) {
+    m_enemy->onUpdate(deltaTime);
     m_player->onUpdate(deltaTime);
 }
 
 void CharacterManager::render(const Camera& camera) {
+    m_enemy->render(camera);
     BulletTimeManager::instance()->postProcess(camera);
     m_player->render(camera);
 }
 
 CharacterManager::CharacterManager() {
+    m_enemy = new Enemy();
     m_player = new Player();
 }
 
